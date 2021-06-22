22cc2f3f90ef9443688e1603bee2c48c6f578ca9
Caio Luna <ccluna@gmail.com>
2021-06-22 16:55:55 -0300

    configure auth firebase & navigation main screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,41 @@
-import loginImg from '../assets/images/illustration.svg'
+import { useHistory } from 'react-router-dom';
+import { auth, firebase } from '../services/firebase'
+
+import illustrationImg from '../assets/images/illustration.svg'
 import letmeaskLogo from '../assets/images/logo.svg'
 import googleLogo from '../assets/images/google-icon.svg'
+import loginIcon from '../assets/images/login.svg'
 
 import { Button } from '../components/Button';
 
 import '../styles/auth.sass'
 
 export function Home() {
+  const history = useHistory()
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+      .then(result => {
+        history.push('/rooms/new')
+        console.log(result)
+      })
+    
+  }
   return (
     <div id="page-auth">
 
       <header>
-        <img src={loginImg} alt="Chatboxes logo" />
+        <img src={illustrationImg} alt="Chatboxes logo" />
         <h1>Toda pergunta tem uma resposta.</h1>
         <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
       </header>
 
       <main>
-
         <div>
           <img className="letmeask-logo" src={letmeaskLogo} alt="Letmeask logo" />
 
-          <button className="google-btn">
+          <button onClick={handleCreateRoom} className="google-btn">
             <img src={googleLogo} alt="G google logo" />
             Crie sua sala com o Google
           </button>
@@ -33,12 +47,11 @@ export function Home() {
                 <input className="room-code" type="text" placeholder="Digite o código da sala" />
               </label>
               <Button className="button" type="submit">
-                Entrar na sala
+                <img className="login" src={loginIcon} alt="login icon" /> Entrar na sala
               </Button>
             </fieldset>
           </form>
         </div>
-
       </main>
 
     </div>
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg'
+import letmeaskLogo from '../assets/images/logo.svg'
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.sass'
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+
+      <header>
+        <img src={illustrationImg} alt="Chatboxes logo" />
+        <h1>Toda pergunta tem uma resposta.</h1>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </header>
+
+      <main>
+        <div>
+          <img className="letmeask-logo" src={letmeaskLogo} alt="Letmeask logo" />
+
+          <h2>Crie uma nova sala</h2>
+
+          <form action="">
+            <label htmlFor="new-room-name">
+              <input className="new-room-name" type="text" placeholder="Nome da sala" />
+            </label>
+            <Button className="button" type="submit">
+              Criar sala
+            </Button>
+            <p>Quer entrar em uma sala já existente? <Link to="/">Clique aqui</Link></p>
+          </form>
+        </div>
+
+      </main>
+
+    </div>
+  );
+}
